fae3d8a57e6c7d6dcba1a1ab6b4e71304396c880
TANK-MARK-V <dvornikov.mark@mail.ru>
2024-04-08 16:58:12 +0700

    Теперь кнопки только на стартовой странице

--- WEB---/main.py
+++ WEB---/main.py
@@ -13,41 +13,44 @@ app.config['SECRET_KEY'] = 'TipoVikipedia'
 
 @app.route('/')
 def home():
-    return render_template('base.html', buttons=True)
+    return render_template('base.html', buttons=True, title='Sleeper')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template('login.html', buttons=False, message='')
+        return render_template('login.html', buttons=False, message='', title='Авторизация')
     login, hashed_password = request.form['login'], functions.hashing_password(request.form['password'])
     db_sess = db_session.create_session()
     user = db_sess.query(users.Users).filter(users.Users.login == login).first()
     if not user:
-        return render_template('login.html', buttons=False, message='Неверный логин')
+        return render_template('login.html', buttons=False, message='Неверный логин', title='Авторизация')
     if user.hashed_password != hashed_password:
-        return render_template('login.html', buttons=False, message='Неверный пароль')
-    return redirect('/my_wall')
+        return render_template('login.html', buttons=False, message='Неверный пароль', title='Авторизация')
+    return redirect('/my_wall', title='Моя страница')
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
-        return render_template('register.html', buttons=False, message='')
+        return render_template('register.html', buttons=False, message='', title='Регистрация')
     login, name, pasword = request.form['login'], request.form['name'], request.form['password']
     if not all((login, name, pasword)):
-        return render_template('regiser.html', buttons=False, message='Все поля должны быть заполнены')
+        return render_template('regiser.html', buttons=False, message='Все поля должны быть заполнены',
+                               title='Регистрация')
     db_sess = db_session.create_session()
     user = db_sess.query(users.Users).filter(users.Users.login == login).first()
     if user:
-        return render_template('register.html', buttons=False, message='Такой логин уже используют')
+        return render_template('register.html', buttons=False, message='Такой логин уже используют',
+                               title='Регистрация')
     user = db_sess.query(users.Users).filter(users.Users.name == name).first()
     if user:
-        return render_template('register.html', buttons=False, message='Такое прозвище уже существует')
+        return render_template('register.html', buttons=False, message='Такое прозвище уже существует',
+                               title='Регистрация')
     making = functions.make_new_user({'login': login, 'name': name, 'password': pasword})
     if making != 'Удачно':
-        return render_template('register.html', buttons=False, message=making)
-    return redirect('/my_wall')
+        return render_template('register.html', buttons=False, message=making, title='Регистрация')
+    return redirect('/my_wall', title='Моя страница')
 
 
 def main():
